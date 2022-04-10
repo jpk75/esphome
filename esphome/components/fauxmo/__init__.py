9458ab333b51cdc36de3361592f88902d0ac000f
jpk <jpk@e-mail.de>
2022-04-10 07:57:56 +0200

    fixes

--- esphome/components/fauxmo/__init__.py
+++ esphome/components/fauxmo/__init__.py
@@ -1,8 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 
-CONF_DEVICE = "device"
+
+CONF_DEVICE = "devices"
 
 fauxmo_component_ns = cg.esphome_ns.namespace("fauxmo")
 FauxmoComponentConstructor = fauxmo_component_ns.class_("FauxmoComponent")
@@ -17,7 +17,7 @@ CONFIG_SCHEMA = cv.Schema(
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(FauxmoComponentConstructor)
     await cg.register_component(var, config)
 
     for device in config.get(CONF_DEVICE, []):
