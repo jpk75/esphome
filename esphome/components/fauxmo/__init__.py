e244f717fdd9b8c303b622694cf01f870fcdffc3
jpk <jpk@e-mail.de>
2022-04-10 20:39:02 +0200

    fixes

--- esphome/components/fauxmo/__init__.py
+++ esphome/components/fauxmo/__init__.py
@@ -1,26 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
-
-CONF_DEVICE = "devices"
+CONF_DEVICES = "devices"
 
 fauxmo_component_ns = cg.esphome_ns.namespace("fauxmo")
-FauxmoComponentConstructor = fauxmo_component_ns.class_("FauxmoComponent")
+FauxmoComponentConstructor = fauxmo_component_ns.class_("FauxmoComponent", cg.Component)
 
 MULTI_CONF = True
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(FauxmoComponentConstructor),
-        cv.Required(CONF_DEVICE): cv.All(cv.string, cv.Length(min=1)),
+        cv.Required(CONF_DEVICES): cv.All(cv.string, cv.Length(min=1)),
     }
 )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(FauxmoComponentConstructor)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    for device in config.get(CONF_DEVICE, []):
-        cg.add(var.register_device(device))
+    #    for device in config.get(CONF_DEVICES, []):
+    #        cg.add(var.register_device(device))
 
+    cg.add(var.register_device(config.get(CONF_DEVICES, [])))
+    cg.add_library("FauxmoESP", "3.4")
     cg.add_define("USE_FAUXMO")
--- esphome/components/fauxmo/fauxmo_component.cpp
+++ esphome/components/fauxmo/fauxmo_component.cpp
@@ -9,7 +9,7 @@ FauxmoComponent::FauxmoComponent(){
 
 }
 
-void FauxmoComponent::setup() override {
+void FauxmoComponent::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Fauxmo...");
 
   // By default, fauxmoESP creates it's own webserver on the defined port
@@ -32,17 +32,17 @@ void FauxmoComponent::setup() override {
   //fauxmo.addDevice(ID_YELLOW);
 
 
-  this->fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+  this->fauxmo.onSetState([&](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
       ESP_LOGV("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-      for(auto it=mapDevices.begin(); it != mapDevices.end(); it++){
-      if (it->first == device_name) {
-          it->second(device_id, device_name, state, value);
+      for(auto& device : this->devices_list){
+      if (device == device_name) {
+          ESP_LOGV("[onSetState] Device '%s' matched", device.c_str());
           return;
       }
       }
   }); 
 }
-void FauxmoComponent::loop() override {
+void FauxmoComponent::loop() {
   // fauxmoESP uses an async TCP server but a sync UDP server
   // Therefore, we have to manually poll for UDP packets
   this->fauxmo.handle();
@@ -53,28 +53,21 @@ void FauxmoComponent::loop() override {
 void FauxmoComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "Fauxmo:");
   ESP_LOGCONFIG(TAG, "Registered Devices:");
-  for(auto& device : devices_list){
+  for(auto& device : this->devices_list){
     ESP_LOGCONFIG(TAG, "  %s", device.c_str());
   }
 }
-float EthernetComponent::get_setup_priority() const { return setup_priority::WIFI; }
-bool EthernetComponent::can_proceed() { return this->is_connected(); }
-network::IPAddress EthernetComponent::get_ip_address() {
-  tcpip_adapter_ip_info_t ip;
-  tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_ETH, &ip);
-  return {ip.ip.addr};
-}
 
-float FauxmoComponent::get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
+float FauxmoComponent::get_setup_priority() const { return esphome::setup_priority::AFTER_WIFI; }
 
 void FauxmoComponent::register_device(const std::string& device_name){
   ESP_LOGCONFIG(TAG, "Register device '%s' ...", device_name.c_str());
-  for(auto it=this->devices_list.begin(); it != this->devices_list.end(); it++)
-      if (it->first == device_name) return;
+  for(auto& device : this->devices_list)
+      if (device == device_name) return;
   this->devices_list.push_back(device_name);
 
   // Add virtual devices
-  this->fauxmo.addDevice(device_name);
+  this->fauxmo.addDevice(device_name.c_str());
 }
 
 } // namespace fauxmo
